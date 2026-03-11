21f2cf31e4aab04fd08e23cb0a5e56764a1da897
pavin <pavin227@gmail.com>
2026-03-11 10:15:10 +0530

    updates: added history route for telemetry for dashboard.

--- backend/src/telemetry/index.ts
+++ backend/src/telemetry/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { getHistoryTelemetry } from "./controllers/telemetryController.js";
+import verifyUser from "../users/controller/userAuthMiddleware.js";
 
 
 const router = Router();
 
-
+router.get('/history', verifyUser, getHistoryTelemetry);
 
 export default router;
